1e1e2d019c1dfcdd6ee9fe540ed6fd25170b3bbe
LeondavidZipp <leondavidzipp@gmx.de>
2025-02-05 11:44:59 +0100

    fixed database to accept connections from other containers

--- src/requirements/mariadb/tools/docker-entrypoint.sh
+++ src/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Get secrets
-WP_DB_USER_PWD=$(cat /run/secrets/wp_db_user_pwd)
-DB_ROOT_PWD=$(cat /run/secrets/db_root_pwd)
+export WP_DB_USER_PWD=$(cat /run/secrets/wp_db_user_pwd)
 
 # Start MariaDB server in the background
 mysqld_safe --skip-networking &
@@ -13,17 +12,13 @@ while ! mysqladmin ping --silent; do
 	sleep 1
 done
 
-# Run database initialization
-sed \
-	-e "s/wp_db_user/$WP_DB_USER/g" \
-	-e "s/wp_db_user_pwd/$WP_DB_USER_PWD/g" \
-	-e "s/wp_db_name/$WP_DB_NAME/g" \
-	-e "s/db_root_pwd/$DB_ROOT_PWD/g" \
-	/tmp/init.sql > /tmp/init.tmp.sql \
+envsubst < /tmp/init.sql > /tmp/init.tmp.sql \
 	&& mysql -u root < /tmp/init.tmp.sql \
+	&& cat /tmp/init.tmp.sql \
 	&& rm -f \
 		/tmp/init.tmp.sql \
 		/tmp/init.sql \
+	&& unset WP_DB_USER_PWD \
 	&& echo "Database initialization done"
 
 # Stop the background MariaDB server
