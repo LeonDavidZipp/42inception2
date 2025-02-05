92f2bf521ce1f145a6245319c5b9f0ae2d965a8c
LeondavidZipp <leondavidzipp@gmx.de>
2025-02-05 15:35:06 +0100

    removed debug statments && unnecessary commands && added status echo messages

--- src/requirements/mariadb/tools/docker-entrypoint.sh
+++ src/requirements/mariadb/tools/docker-entrypoint.sh
@@ -4,6 +4,7 @@
 export WP_DB_USER_PWD=$(cat /run/secrets/wp_db_user_pwd)
 
 # Start MariaDB server in the background
+echo "Starting MariaDB for initialization..."
 mysqld_safe --skip-networking &
 mariadb_pid=$!
 
@@ -12,20 +13,21 @@ while ! mysqladmin ping --silent; do
 	sleep 1
 done
 
+echo "Initializing MariaDB..."
 envsubst < /tmp/init.sql > /tmp/init.tmp.sql \
 	&& mysql -u root < /tmp/init.tmp.sql \
-	&& cat /tmp/init.tmp.sql \
 	&& rm -f \
 		/tmp/init.tmp.sql \
 		/tmp/init.sql \
 	&& unset WP_DB_USER_PWD \
-	&& echo "Database initialization done"
+	&& echo "MariaDB initialization done..."
 
 # Stop the background MariaDB server
 mysqladmin shutdown --silent
 
 # Initialize database structure OR upgrade database
-mysql_install_db > /dev/null || mysql_upgrade
+mysql_install_db > /dev/null 2>&1 || mysql_upgrade > /dev/null 2>&1
 
 # Start MariaDB
+echo "Starting MariaDB..."
 mysqld
--- src/requirements/wordpress/tools/docker-entrypoint.sh
+++ src/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,64 +1,50 @@
 #!/bin/bash
 
 # Create config if it doesn't exist
-#if [ ! -f /var/www/html/wp-config.php ]; then
-    cd /var/www/html
-    wp core download --allow-root
+cd /var/www/html
+wp core download --allow-root
 
-    WP_DB_USER_PWD=$(cat /run/secrets/wp_db_user_pwd)
-    WP_ADMIN_PWD=$(cat /run/secrets/wp_admin_pwd)
-    WP_USER_PWD=$(cat /run/secrets/wp_user_pwd)
+WP_DB_USER_PWD=$(cat /run/secrets/wp_db_user_pwd)
+WP_ADMIN_PWD=$(cat /run/secrets/wp_admin_pwd)
+WP_USER_PWD=$(cat /run/secrets/wp_user_pwd)
 
-    echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-    echo WP_ADMIN_PWD: $WP_ADMIN_PWD
-    echo WP_USER_PWD: $WP_USER_PWD
+echo "Creating wp-config.php..."
+wp config create \
+	--dbname="${WP_DB_NAME}" \
+	--dbuser="${WP_DB_USER}" \
+	--dbpass="${WP_DB_USER_PWD}" \
+	--dbhost="${WP_DB_HOST}" \
+	--dbprefix=${WP_DB_PREFIX} \
+	--config-file=/var/www/html/wp-config.php \
+	--force \
+	--allow-root
 
-    echo WP_DB_NAME: $WP_DB_NAME
-    echo WP_DB_USER: $WP_DB_USER
-    echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-    echo WP_DB_HOST: $WP_DB_HOST
-    echo WP_DB_PREFIX: $WP_DB_PREFIX
+echo
+if ! wp core is-installed --allow-root; then
+	echo "Installing WordPress core..."
+	wp core install \
+		--url="${WP_DOMAIN}" \
+		--title="${WP_TITLE}" \
+		--admin_user="${WP_ADMIN}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		--admin_password="${WP_ADMIN_PWD}" \
+		--allow-root
+else
+	echo "WordPress is already installed. Skipping core installation."
+fi
 
-    wp config create \
-        --dbname="${WP_DB_NAME}" \
-        --dbuser="${WP_DB_USER}" \
-        --dbpass="${WP_DB_USER_PWD}" \
-        --dbhost="${WP_DB_HOST}" \
-        --dbprefix=${WP_DB_PREFIX} \
-        --config-file=/var/www/html/wp-config.php \
-        --force \
-        --allow-root
-
-    echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-
-    if ! wp core is-installed --allow-root; then
-        wp core install \
-            --url="${WP_DOMAIN}" \
-            --title="${WP_TITLE}" \
-            --admin_user="${WP_ADMIN}" \
-            --admin_email="${WP_ADMIN_EMAIL}" \
-            --admin_password="${WP_ADMIN_PWD}" \
-            --allow-root
-    else
-    	echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-        echo "WordPress is already installed. Skipping core installation."
-    fi
-
-	echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-
-    if ! wp user get "${WP_USER}" --field=ID --allow-root > /dev/null 2>&1; then
-        wp user create \
-            "${WP_USER}" \
-            "${WP_USER_EMAIL}" \
-            --user_pass="${WP_USER_PWD}" \
-            --allow-root
-    else
-    	echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-        echo "User ${WP_USER} already exists. Skipping user creation."
-    fi
-    echo WP_DB_USER_PWD: $WP_DB_USER_PWD
-#fi
+if ! wp user get "${WP_USER}" --field=ID --allow-root > /dev/null 2>&1; then
+	echo "Creating user ${WP_USER}..."
+	wp user create \
+		"${WP_USER}" \
+		"${WP_USER_EMAIL}" \
+		--user_pass="${WP_USER_PWD}" \
+		--allow-root
+else
+	echo "User ${WP_USER} already exists. Skipping user creation."
+fi
 
 chown -R www-data:www-data /var/www/html/
 
+echo "Starting PHP-FPM..."
 php-fpm8.2 -F
